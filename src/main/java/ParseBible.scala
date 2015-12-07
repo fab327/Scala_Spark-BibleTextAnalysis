8e1b9acdf3a1a6b1c143b22af19c3b568b26d701
Fab Desktop <spiritualsilver@gmail.com>
2015-12-07 00:36:20 -0500

    Update to project

---
+++ src/main/java/ParseBible.scala
@@ -0,0 +1,120 @@
+import java.io.{FileOutputStream, PrintStream}
+import java.util.Properties
+
+import edu.stanford.nlp.ling.CoreAnnotations.{LemmaAnnotation, SentencesAnnotation, TokensAnnotation}
+import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.rdd.RDD
+
+import scala.collection.JavaConverters._
+import scala.collection.Map
+import scala.collection.mutable.{ArrayBuffer, HashMap}
+
+object ParseBible {
+
+  def loadStopWords(path: String) = scala.io.Source.fromFile(path).getLines().toSet
+
+  def createNLPPipeline(): StanfordCoreNLP = {
+    val props = new Properties()
+    props.put("annotators", "tokenize, ssplit, pos, lemma")
+    new StanfordCoreNLP(props)
+  }
+
+  def plainTextToLemmas(text: String, stopWords: Set[String], pipeline: StanfordCoreNLP)
+  : Seq[String] = {
+    val doc = new Annotation(text)
+    pipeline.annotate(doc)
+    val lemmas = new ArrayBuffer[String]()
+    val sentences = doc.get(classOf[SentencesAnnotation])
+    for (sentence <- sentences.asScala;
+         token <- sentence.get(classOf[TokensAnnotation]).asScala) {
+      val lemma = token.get(classOf[LemmaAnnotation])
+      if (lemma.length > 2 && !stopWords.contains(lemma) && isOnlyLetters(lemma)) {
+        //Not converting lemma to lowercase because the Bible contains names that shouldn't be so
+        lemmas += lemma
+      }
+    }
+    lemmas
+  }
+
+  def isOnlyLetters(str: String): Boolean = {
+    // While loop for high performance
+    var i = 0
+    while (i < str.length) {
+      if (!Character.isLetter(str.charAt(i))) {
+        return false
+      }
+      i += 1
+    }
+    true
+  }
+
+  /**
+    * Returns a term-document matrix where each element is the TF-IDF of the row's document and
+    * the column's term.
+    */
+  def termDocumentMatrix(docs: RDD[(String, Seq[String])], stopWords: Set[String], numTerms: Int,
+                         sc: SparkContext): (RDD[Vector], Map[Int, String], Map[Long, String], Map[String, Double]) = {
+
+    //REQUIREMENT #2: Computes TF-IDS
+    val docTermFreqs = docs.mapValues(terms => {
+      val termFreqsInDoc = terms.foldLeft(new HashMap[String, Int]()) {
+        (map, term) => map += term -> (map.getOrElse(term, 0) + 1)
+      }
+      termFreqsInDoc
+    })
+
+    docTermFreqs.cache()
+    val docIds = docTermFreqs.map(_._1).zipWithUniqueId().map(_.swap).collectAsMap()
+
+    val docFreqs = documentFrequenciesDistributed(docTermFreqs.map(_._2), numTerms)
+    println("Number of terms: " + docFreqs.size)
+    saveDocFreqs(".docfreqs.tsv", docFreqs)
+
+    val numDocs = docIds.size
+
+    val idfs = inverseDocumentFrequencies(docFreqs, numDocs)
+
+    // Maps terms to their indices in the vector
+    val idTerms = idfs.keys.zipWithIndex.toMap
+    val termIds = idTerms.map(_.swap)
+
+    val bIdfs = sc.broadcast(idfs).value
+    val bIdTerms = sc.broadcast(idTerms).value
+
+    val vecs = docTermFreqs.map(_._2).map(termFreqs => {
+      val docTotalTerms = termFreqs.values.sum
+      val termScores = termFreqs.filter {
+        case (term, freq) => bIdTerms.contains(term)
+      }.map {
+        case (term, freq) => (bIdTerms(term), bIdfs(term) * termFreqs(term) / docTotalTerms)
+      }.toSeq
+      Vectors.sparse(bIdTerms.size, termScores)
+    })
+    (vecs, termIds, docIds, idfs)
+  }
+
+  def saveDocFreqs(path: String, docFreqs: Array[(String, Int)]) {
+    val ps = new PrintStream(new FileOutputStream(path))
+    for ((doc, freq) <- docFreqs) {
+      ps.println(s"$doc\t$freq")
+    }
+    ps.close()
+  }
+
+  def documentFrequenciesDistributed(docTermFreqs: RDD[HashMap[String, Int]], numTerms: Int)
+  : Array[(String, Int)] = {
+    val docFreqs = docTermFreqs.flatMap(_.keySet).map((_, 1)).reduceByKey(_ + _, 15)
+    val ordering = Ordering.by[(String, Int), Int](_._2)
+    docFreqs.top(numTerms)(ordering)
+  }
+
+  def inverseDocumentFrequencies(docFreqs: Array[(String, Int)], numDocs: Int)
+  : Map[String, Double] = {
+    docFreqs.map { case (term, count) => (term, math.log(numDocs.toDouble / count)) }.toMap
+  }
+
+}
+
